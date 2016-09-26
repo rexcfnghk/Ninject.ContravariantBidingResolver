4a049542cfdb4cb0da81149866af5272aef5425e
Rex Ng <rexcfnghk@gmail.com>
2016-09-26 22:25:51 +0800

    Initial commit

---
+++ ContravariantBindingResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ninject.Components;
+using Ninject.Infrastructure;
+using Ninject.Planning.Bindings;
+using Ninject.Planning.Bindings.Resolvers;
+
+namespace Ninject.ContravariantBindingResolver
+{
+    public class ContravariantBindingResolver : NinjectComponent, IBindingResolver
+    {
+        /// <summary>
+        /// Returns any bindings from the specified collection that match the specified service.
+        /// </summary>
+        public IEnumerable<IBinding> Resolve(Multimap<Type, IBinding> bindings, Type service)
+        {
+            if (!service.IsGenericType)
+            {
+                return Enumerable.Empty<IBinding>();
+            }
+
+            Type genericType = service.GetGenericTypeDefinition();
+            Type[] genericArguments = genericType.GetGenericArguments();
+            if (genericArguments.Length != 1 ||
+                !genericArguments[0].GenericParameterAttributes.HasFlag(GenericParameterAttributes.Contravariant))
+            {
+                return Enumerable.Empty<IBinding>();
+            }
+
+            Type argument = service.GetGenericArguments().Single();
+            return from kvp in bindings
+                from value in kvp.Value
+                let key = kvp.Key
+                let genericArgument = key.GetGenericArguments().Single()
+                where
+                key.IsGenericType && key.GetGenericTypeDefinition() == genericType && genericArgument != argument &&
+                genericArgument.IsAssignableFrom(argument)
+                select value;
+        }
+    }
+}
