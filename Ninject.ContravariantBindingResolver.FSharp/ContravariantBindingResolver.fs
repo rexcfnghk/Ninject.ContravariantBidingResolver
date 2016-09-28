2e20329587717165f8cc4e6d7fa28698f0bd1e18
Rex Ng <rexcfnghk@gmail.com>
2016-09-28 22:30:35 +0800

    Add F# version for comparison

---
+++ Ninject.ContravariantBindingResolver.FSharp/ContravariantBindingResolver.fs
@@ -0,0 +1,23 @@
+﻿namespace Ninject.ContravariantBindingResolver.FSharp
+
+open System.Reflection
+open Ninject.Components
+open Ninject.Planning.Bindings.Resolvers
+
+type ContravariantBindingResolver () =
+    inherit NinjectComponent ()
+    interface IBindingResolver with
+        member __.Resolve(bindings, service) =
+            if not service.IsGenericType
+            then Seq.empty
+            else
+                let genericType = service.GetGenericTypeDefinition ()
+                let genericArguments = genericType.GetGenericArguments ()
+                if genericArguments.Length = 1 || not <| genericArguments.[0].GenericParameterAttributes.HasFlag GenericParameterAttributes.Contravariant
+                then Seq.empty
+                else
+                    let argument = service.GetGenericArguments () |> Array.exactlyOne
+                    bindings
+                    |> Seq.map (fun (KeyValue (k, v)) -> k, k.GetGenericArguments () |> Array.exactlyOne, v)
+                    |> Seq.filter (fun (key, genericArgument, _) -> key.IsGenericType && key.GetGenericTypeDefinition() = genericType && genericArgument <> argument && genericArgument.IsAssignableFrom argument)
+                    |> Seq.collect (fun (_, _, v) -> v)
