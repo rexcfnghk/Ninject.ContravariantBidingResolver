fd349339d2e3491a956085ca0bdd1141a72e8128
Rex Ng <rexcfnghk@gmail.com>
2016-09-28 22:40:55 +0800

    Formatting update

--- Ninject.ContravariantBindingResolver.FSharp/ContravariantBindingResolver.fs
+++ Ninject.ContravariantBindingResolver.FSharp/ContravariantBindingResolver.fs
@@ -7,17 +7,24 @@ open Ninject.Planning.Bindings.Resolvers
 type ContravariantBindingResolver () =
     inherit NinjectComponent ()
     interface IBindingResolver with
-        member __.Resolve(bindings, service) =
+        member __.Resolve (bindings, service) =
             if not service.IsGenericType
             then Seq.empty
             else
                 let genericType = service.GetGenericTypeDefinition ()
                 let genericArguments = genericType.GetGenericArguments ()
-                if genericArguments.Length = 1 || not <| genericArguments.[0].GenericParameterAttributes.HasFlag GenericParameterAttributes.Contravariant
+                if genericArguments.Length = 1 || 
+                    GenericParameterAttributes.Contravariant
+                    |> genericArguments.[0].GenericParameterAttributes.HasFlag
+                    |> not
                 then Seq.empty
                 else
                     let argument = service.GetGenericArguments () |> Array.exactlyOne
                     bindings
                     |> Seq.map (fun (KeyValue (k, v)) -> k, k.GetGenericArguments () |> Array.exactlyOne, v)
-                    |> Seq.filter (fun (key, genericArgument, _) -> key.IsGenericType && key.GetGenericTypeDefinition() = genericType && genericArgument <> argument && genericArgument.IsAssignableFrom argument)
+                    |> Seq.filter (fun (key, genericArgument, _) -> 
+                        key.IsGenericType && 
+                        key.GetGenericTypeDefinition () = genericType &&
+                        genericArgument <> argument && 
+                        genericArgument.IsAssignableFrom argument)
                     |> Seq.collect (fun (_, _, v) -> v)
